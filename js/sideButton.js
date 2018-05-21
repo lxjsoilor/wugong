c40b776910e36bb4e9339d93642e93fa7fdce84f
sailor <416596401@qq.com>
2018-05-21 17:51:40 +0800

    453

--- js/sideButton.js
+++ js/sideButton.js
@@ -1,5 +1,5 @@
 $(function () {
-	var topHtml = '<div id="izl_rmenu" class="izl-rmenu"><a href="http://wpa.qq.com/msgrd?v=3&amp;uin=2395278733&amp;site=http://www.estronger.cn/&amp;menu=yes" style="background-color:#ff8314;"title="279759361" target="blank" class="btn btn-qq"><i class="glyphicon glyphicon-comment" style="font-size:30px;line-height:1.5; "></i><p class="text-center congHidden" style="font-size:12px">在线客服</p></a>\
+	var topHtml = '<div id="izl_rmenu" class="izl-rmenu"><a href="http://wpa.qq.com/msgrd?v=3&amp;uin=416596401&amp;site=http://www.estronger.cn/&amp;menu=yes" style="background-color:#ff8314;"title="279759361" target="blank" class="btn btn-qq"><i class="glyphicon glyphicon-comment" style="font-size:30px;line-height:1.5; "></i><p class="text-center congHidden" style="font-size:12px">在线客服</p></a>\
 					<div class="btn btn-phone"><i class="glyphicon glyphicon-earphone" style="font-size:30px;line-height:1.5;"></i>\
 						<p class="text-center congHidden" style="font-size:12px">咨询电话</p>\
 						<div class="phone"><a target="_blank" href="tel:13925817311">13925817311</a></div>\
